061f056ae9552a01b317b8fe281291dfc462de51
VTG23 <146595365+VTG23@users.noreply.github.com>
2023-10-01 10:17:01 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const botoes = document.querySelectorAll(".botao");
+
+const personagens = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarbotao();
+        desselecionarpersonagem();
+
+        botao.classList.add("selecionado");
+
+
+
+        personagens[indice].classList.add("selecionado");
+    });
+
+});
+function desselecionarpersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
